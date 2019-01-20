f3b5b2360db75eba1fc2c849757b06d6d4263dda
rgruebel <r.gruebel@bitbeans.de>
2019-01-20 14:21:10 +0100

    Update Button hinzugefügt der die Karte mithilfe eines Webhook aktualisieren kann.

--- custom_components/zigbee2mqtt_networkmap.py
+++ custom_components/zigbee2mqtt_networkmap.py
@@ -9,33 +9,41 @@ CONF_TOPIC = 'topic'
 DEFAULT_TOPIC = 'zigbee2mqtt'
 
 
-def setup(hass, config):
+async def async_setup(hass, config):
     """Set up the zigbee2mqtt_networkmap component."""
     mqtt = hass.components.mqtt
     topic = config[DOMAIN].get(CONF_TOPIC, DEFAULT_TOPIC)
     entity_id = 'zigbee2mqtt_networkmap.map_last_update'
 
+    async def handle_webhook(hass, webhook_id, request):
+        """Handle webhook callback."""
+        mqtt.async_publish(topic+'/bridge/networkmap', 'graphviz')
+    # Register the Webhook
+    webhook_id = hass.components.webhook.async_generate_id()
+    hass.components.webhook.async_register(DOMAIN, 'zigbee2mqtt_networkmap webhook', webhook_id, handle_webhook)
+    webhook_url = hass.components.webhook.async_generate_url(webhook_id)
+
     # Listener to be called when we receive a message.
-    def message_received(topic, payload, qos):
+    async def message_received(topic, payload, qos):
         """Handle new MQTT messages."""
         # Save Response as JS variable in source.js
         last_update = datetime.now()
         f = open(hass.config.path('www', 'zigbee2mqtt_networkmap', 'source.js'),"w")
-        f.write("var last_update = new Date('"+last_update.strftime('%Y-%m-%d %H:%M:%S.%f')+"');\nvar graph = \'"+payload.replace('\n', ' ').replace('\r', '')+"\'")
+        f.write("var webhook = '"+webhook_url+"';\nvar last_update = new Date('"+last_update.strftime('%Y-%m-%d %H:%M:%S.%f')+"');\nvar graph = \'"+payload.replace('\n', ' ').replace('\r', '')+"\'")
         f.close()
-        hass.states.set(entity_id, last_update)
+        hass.states.async_set(entity_id, last_update)
 
     # Subscribe our listener to the networkmap topic.
-    mqtt.subscribe(topic+'/bridge/networkmap/graphviz', message_received)
+    await mqtt.async_subscribe(topic+'/bridge/networkmap/graphviz', message_received)
 
     # Set the initial state.
-    hass.states.set(entity_id, None)
+    hass.states.async_set(entity_id, None)
 
-    # Service to publish a message on MQTT.
-    def update_service(call):
+    #Service to publish a message on MQTT.
+    async def update_service(call):
         """Service to send a message."""
-        mqtt.publish(topic+'/bridge/networkmap', 'graphviz')
+        mqtt.async_publish(topic+'/bridge/networkmap', 'graphviz')
 
-    hass.services.register(DOMAIN, 'update', update_service)
-    
+    hass.services.async_register(DOMAIN, 'update', update_service)
     return True
+
